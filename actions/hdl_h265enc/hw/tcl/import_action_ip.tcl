287c36f36ac94be7472b69e6ed884992d68fc032
Yanheng <lyhlu@cn.ibm.com>
2018-09-13 11:07:13 +0200

    use another IP generation method

--- actions/hdl_h265enc/hw/tcl/import_action_ip.tcl
+++ actions/hdl_h265enc/hw/tcl/import_action_ip.tcl
@@ -1,18 +1,14 @@
-set action_ipdir $::env(ACTION_ROOT)/ip
-#add_files -scan_for_includes -norecurse $action_hw
+set action_ipdir $::env(ACTION_ROOT)/ip/action_ip_prj/action_ip_prj.srcs/sources_1/ip
 
 #User IPs
-foreach usr_ip [list \
-                $action_ipdir/fifo_128_512  \
-                $action_ipdir/fifo_64_512  \
-                $action_ipdir/fifo_512_128  \
-               ] {
-  foreach usr_ip_xci [exec find $usr_ip -name *.xci] {
-    puts "                        importing user IP $usr_ip_xci (in PIE core)"
-    add_files -norecurse $usr_ip_xci >> $log_file
-    export_ip_user_files -of_objects  [get_files "$usr_ip_xci"] -force >> $log_file
-  }
+foreach usr_ip [glob -nocomplain -dir $action_ipdir *] {
+    foreach usr_ip_xci [exec find $usr_ip -name *.xci] {
+        set ip_name [file rootname [file tail $usr_ip_xci]]
+        puts "                        importing user IP $ip_name (in hdl_scatter_gather)"
+        add_files -norecurse $usr_ip_xci >> $log_file
+        set_property generate_synth_checkpoint false [ get_files $usr_ip_xci] >> $log_file
+        export_ip_user_files -of_objects             [ get_files $usr_ip_xci] -no_script -sync -force -quiet >> $log_file
+    }
 }
-
 ##temp:xdc
 add_files $::env(ACTION_ROOT)/hw/tcl/timing.xdc >> $log_file

35e9391245b181fac1707077569e32c717bf4688
YTW <18212020120@fudan.edu.cn>
2018-10-15 20:20:57 +0800

    corrct some mistakes
    Signed-off-by: YTW <18212020120@fudan.edu.cn>

--- actions/hdl_h265enc/sw/hdl_h265enc.h
+++ actions/hdl_h265enc/sw/hdl_h265enc.h
@@ -41,7 +41,7 @@
 #define FRAMEWIDTH              2560
 #define FRAMEHEIGHT             1600
 #define X_TOTAL                 (FRAMEWIDTH /64)-1
-#define Y_TOTAL                 (FRAMEWIDTH /64)-1
+#define Y_TOTAL                 (FRAMEHEIGHT /64)-1
 #define QP                      0x00000016
 #define REC_0_BASE              (FRAMEWIDTH*FRAMEHEIGHT*3/2)*1 
 #define REC_1_BASE              (FRAMEWIDTH*FRAMEHEIGHT*3/2)*2
